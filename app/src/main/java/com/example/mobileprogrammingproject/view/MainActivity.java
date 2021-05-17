73dae9ac6879cd0358e26cc5d5245dbe9af4d97f
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2021-05-17 10:02:33 +0900

    Add navigation drawer

--- app/src/main/java/com/example/mobileprogrammingproject/view/MainActivity.java
+++ app/src/main/java/com/example/mobileprogrammingproject/view/MainActivity.java
@@ -3,14 +3,23 @@ package com.example.mobileprogrammingproject.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import com.bumptech.glide.Glide;
 import com.example.mobileprogrammingproject.R;
 import com.example.mobileprogrammingproject.databinding.ActivityMainBinding;
+import com.example.mobileprogrammingproject.databinding.ActivityMainDrawerBinding;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -27,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
     private MyPageFragment myPageFragment;
     private SettingFragment settingFragment;
 
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+    private Button btn_close;
+    private ImageView profileImg;
+    private TextView tv_name, tv_email;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,12 +55,53 @@ public class MainActivity extends AppCompatActivity {
         strProfileImg = intent.getStringExtra("profileImg");
         strEmailType = intent.getStringExtra("check");
 
+        viewInit();
+
+        btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "열림", Toast.LENGTH_SHORT).show();
+                drawerLayout.closeDrawers();
+            }
+        });
+        DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        };
+
+        drawerLayout.setDrawerListener(listener);
+        drawerView.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return true;
+            }
+        });
+
+
         mBinding.bottomNavi.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()){
                     case R.id.action_menu:
-                        setFrag(0);
+                        drawerLayout.openDrawer(drawerView);
                         break;
                     case R.id.action_search:
                         setFrag(1);
@@ -69,6 +125,19 @@ public class MainActivity extends AppCompatActivity {
         myPageFragment = new MyPageFragment();
         settingFragment = new SettingFragment();
         setFrag(2); // 첫 프래그먼트 화면을 무엇으로 지정해줄 것인지 선택
+    }
+
+    private void viewInit() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        drawerView = findViewById(R.id.drawer);
+        btn_close = findViewById(R.id.btn_close);
+        profileImg = findViewById(R.id.iv_profile);
+        tv_name = findViewById(R.id.tv_nickName);
+        tv_email = findViewById(R.id.tv_email);
+
+        Glide.with(this).load(strProfileImg).override(300,300).into(profileImg);
+        tv_name.setText(strNick);
+        tv_email.setText(strEmail);
     }
 
     // 프래그먼트 교체가 일어나는 실행문이다, 총 5개의 fragment 교체
