9432ffa38c68351304bad52b85516f4f3645d02d
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2021-05-26 17:25:59 +0900

    Refactoring

--- app/src/main/java/com/example/mobileprogrammingproject/api/VaccinationApi.java
+++ app/src/main/java/com/example/mobileprogrammingproject/api/VaccinationApi.java
@@ -17,7 +17,7 @@ import retrofit2.http.QueryMap;
 
 public interface VaccinationApi {
 
-    //@Headers({"accept: application/json", "Authorization: 91L7g5h4Uzl%2FlRV34MsnMRF2uLY%2F4lW3STU2J%2Bawycm38VIzk5f6ErydPZ78TfJY6eyJMFW8x7vR07wMZsKHcA%3D%3D"})
+    @Headers({"accept: application/json", "Authorization: 91L7g5h4Uzl%2FlRV34MsnMRF2uLY%2F4lW3STU2J%2Bawycm38VIzk5f6ErydPZ78TfJY6eyJMFW8x7vR07wMZsKHcA%3D%3D"})
     @GET("15077586/v1/centers?page=1&perPage=263&returnType=JSON&serviceKey=91L7g5h4Uzl%2FlRV34MsnMRF2uLY%2F4lW3STU2J%2Bawycm38VIzk5f6ErydPZ78TfJY6eyJMFW8x7vR07wMZsKHcA%3D%3D")
     Call<Pagination<List<Center>>> getCenter();
 }
